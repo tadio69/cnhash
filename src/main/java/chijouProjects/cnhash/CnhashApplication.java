e9b0089ec31127dd8c4fb061dff0fdcf6193e8a6
dubois <duro.chijou@gmail.com>
2026-02-20 19:52:05 +0100

    Commit du 20-02-2026

---
+++ src/main/java/chijouProjects/cnhash/CnhashApplication.java
@@ -0,0 +1,13 @@
+package chijouProjects.cnhash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnhashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CnhashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chijouProjects/cnhash/CnhashApplicationTests.java
@@ -0,0 +1,13 @@
+package chijouProjects.cnhash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnhashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
